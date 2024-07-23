bef56a376f6c107e762c5bed9ac13382e3cbcb20
v4violet <55338035+v4violet@users.noreply.github.com>
2024-07-23 15:37:24 -0400

    add ping times to status

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/disgoorg/disgo"
@@ -61,6 +62,17 @@ func init() {
 	vote_pin.Init()
 }
 
+func createStatus(latency *time.Duration) gateway.PresenceOpt {
+	if latency != nil {
+		l := latency.Round(time.Microsecond * 10)
+		latency = &l
+	}
+	return gateway.WithCustomActivity(templates.Exec("generic.status", map[string]any{
+		"GitRevision": config.Config.GitRevision,
+		"Latency":     latency,
+	}))
+}
+
 func main() {
 	clientConfig := []bot.ConfigOpt{
 		bot.WithGatewayConfigOpts(
@@ -75,6 +87,10 @@ func main() {
 				slog.String("user_id", event.User.ID.String()),
 				slog.String("user_tag", event.User.Tag()),
 			)
+			latency := event.Client().Gateway().Latency()
+			if err := event.Client().SetPresence(context.Background(), createStatus(&latency)); err != nil {
+				slog.Warn("error setting presence")
+			}
 		}),
 		bot.WithEventListenerFunc(func(event *events.GuildsReady) {
 			slog.Info("guilds ready", slog.Int("count", event.Client().Caches().GuildsLen()))
@@ -82,10 +98,16 @@ func main() {
 		bot.WithEventListenerFunc(func(event *events.Resumed) {
 			slog.Info("resumed", slog.Int("sequence", event.SequenceNumber()))
 		}),
+		bot.WithEventListenerFunc(func(event *events.HeartbeatAck) {
+			latency := event.Client().Gateway().Latency()
+			if err := event.Client().SetPresence(context.Background(), createStatus(&latency)); err != nil {
+				slog.Warn("error setting presence")
+			}
+		}),
 	}
 
 	if config.Config.GitRevision != nil {
-		clientConfig = append(clientConfig, bot.WithGatewayConfigOpts(gateway.WithPresenceOpts(gateway.WithCustomActivity((*config.Config.GitRevision)[:7]))))
+		clientConfig = append(clientConfig, bot.WithGatewayConfigOpts(gateway.WithPresenceOpts(createStatus(nil))))
 	}
 
 	enabledModules := []string{}
