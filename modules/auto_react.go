f13eb13274751fa4af83414554f11784e0deecac
v4violet <55338035+v4violet@users.noreply.github.com>
2024-09-19 14:00:20 -0400

    feat(modules/auto_react): add yoai and yuri to boykisser and girlkisser auto react triggers

--- modules/auto_react.go
+++ modules/auto_react.go
@@ -19,8 +19,8 @@ import (
 var auto_reactions_raw = map[string]string{
 	"(?i)(penis|cock|dick|cum)":      "cum:1144596862075154562",
 	"(?i)(ch|chapter\\s*)2":          "ch2_wen:1144404092093997056",
-	"(?i)boy(s|kisser)":              "boykisser:1156664341286899772",
-	"(?i)girl(s|kisser)":             "girlkisser:1202306410352738354",
+	"(?i)(boy(s|kisser)|yoai)":       "boykisser:1156664341286899772",
+	"(?i)(girl(s|kisser)|yuri)":      "girlkisser:1202306410352738354",
 	"(?i)cop(e|i(um|ng))":            "COPIUM:1144404181000671354",
 	"(?i)cucumber":                   "cucumber:1237250194089971712",
 	"(?i)sus":                        "amogus:1144404244615659590",
